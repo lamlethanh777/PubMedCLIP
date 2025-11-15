98acdbc165d1d5a1c80003ca574f277532a8a9ac
Lê Thanh Lâm <lamlethanh177@gmail.com>
2025-11-15 21:38:07 +0700

    fix: `inplace=True` affects gradient calculation

--- QCR_PubMedCLIP/main/classifier.py
+++ QCR_PubMedCLIP/main/classifier.py
@@ -22,7 +22,7 @@ class SimpleClassifier(nn.Module):
         layers = [
             weight_norm(nn.Linear(in_dim, hid_dim), dim=None),
             activation_func,
-            nn.Dropout(cfg.TRAIN.DROPOUT, inplace=True),
+            nn.Dropout(cfg.TRAIN.DROPOUT, inplace=False),
             weight_norm(nn.Linear(hid_dim, out_dim), dim=None)
         ]
         self.main = nn.Sequential(*layers)
