2436342fde6521c476620cd1af24851b058674a2
Lê Thanh Lâm <lamlethanh177@gmail.com>
2025-11-15 20:47:11 +0700

    debug

--- QCR_PubMedCLIP/lib/dataset/dataset_RAD.py
+++ QCR_PubMedCLIP/lib/dataset/dataset_RAD.py
@@ -150,8 +150,8 @@ def _load_dataset(dataroot, name, img_id2val, label2ans):
     entries = []
     for sample, answer in zip(samples, answers):
         utils.assert_eq(sample['qid'], answer['qid'])
+        print(sample['image_name'], answer['image_name'])
         utils.assert_eq(sample['image_name'], answer['image_name'])
-        print(sample['qid'])
         img_id = sample['image_name']
         if not COUNTING_ONLY or is_howmany(sample['question'], answer, label2ans):
             entries.append(_create_entry(img_id2val[img_id], sample, answer))
